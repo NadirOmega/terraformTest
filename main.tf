9c96a3306af3741f8c0d30626aaf8e0e850d8688
Nadir Nadjem <nadjemnadir47@gmail.com>
2020-11-03 12:56:35 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,75 @@
+variable "ibm_api_key" {
+  description = "API key."
+}
+
+
+
+
+provider "ibm" {
+  ibmcloud_api_key = var.ibm_api_key
+}
+
+data ibm_resource_group group {
+  name = "ITGP_DATA"
+}
+
+#data ibm_container_cluster k8s {
+#  resource_group_id = data.ibm_resource_group.group.id
+#  cluster_name_id = "kb-bnpp-itgp-data06-yl-kc-dev"
+#}
+##############################################################################
+
+
+##############################################################################
+# Cluster Data
+##############################################################################
+
+data ibm_container_cluster_config cluster {
+  cluster_name_id   = "kb-bnpp-itgp-data06-yl-kc-dev"
+  resource_group_id = data.ibm_resource_group.group.id
+  admin             = true
+}
+
+##############################################################################
+
+
+##############################################################################
+# Kubernetes Provider
+##############################################################################
+
+provider kubernetes {
+  load_config_file       = false
+  host                   = data.ibm_container_cluster_config.cluster.host
+  client_certificate     = data.ibm_container_cluster_config.cluster.admin_certificate
+  client_key             = data.ibm_container_cluster_config.cluster.admin_key
+  cluster_ca_certificate = data.ibm_container_cluster_config.cluster.ca_certificate
+}
+
+
+##############################################################################
+# Kubernetes Provider
+##############################################################################
+
+provider "helm" {
+  kubernetes {
+  config_path = "${data.ibm_container_cluster_config.cluster.config_file_path}"
+  load_config_file       = false
+    host                   = data.ibm_container_cluster_config.cluster.host
+    client_certificate     = data.ibm_container_cluster_config.cluster.admin_certificate
+    client_key             = data.ibm_container_cluster_config.cluster.admin_key
+    cluster_ca_certificate = data.ibm_container_cluster_config.cluster.ca_certificate
+}
+}
+
+
+ 
+resource "helm_release" "cassandra" {
+  # depends_on = ["data.ibm_container_cluster.k8s"]
+   name      = "CassandraTestHelm"
+   force_update= true
+     wait       = true
+  timeout    = 200
+   repository = "https://charts.bitnami.com/bitnami"
+   chart     = "bitnami/cassandra"
+   namespace = "ververica32"
+}
