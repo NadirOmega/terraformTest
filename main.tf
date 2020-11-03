6ce24394cbfb3753ea10adf3b5c4bb67aef7e1f7
Nadir Nadjem <nadjemnadir47@gmail.com>
2020-11-03 17:05:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,8 +59,8 @@ provider "helm" {
 
  
 resource "helm_release" "cassandra" {
-   create_namespace =true
-   namespace = "test"
+   #create_namespace =true
+   #namespace = "test"
    #depends_on = ["data.ibm_container_cluster.k8s"]
    name      = "cassandratestwarm"
    repository = "https://charts.bitnami.com/ibm"
